0846bb6dce40d8e8f756db11b54d8ccf84a54c2f
chayandasgupta <chayandas285178@gmail.com>
2024-05-04 17:41:51 +0600

    some issue solved & modify sone file

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,18 +3,17 @@
 namespace App\Models;
 
 use App\Constants\MediaCollectionName;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
-use Illuminate\Support\Str;
 
 class Post extends Model implements HasMedia
 {
-    use HasFactory;
-    use InteractsWithMedia;
+    use HasFactory, HasUuids, InteractsWithMedia;
 
 
     /**
@@ -24,7 +23,6 @@ class Post extends Model implements HasMedia
      */
     protected $fillable = [
         'user_id',
-        'uuid',
         'description',
         'view_count'
     ];
@@ -33,13 +31,6 @@ class Post extends Model implements HasMedia
     public function getPostImage()
     {
         return $this->getFirstMediaUrl(MediaCollectionName::POST_IMAGE);
-    }
-
-    public function scopeWithUserAndComments($query)
-    {
-        return $query->select('id', 'description', 'uuid', 'user_id')
-            ->withCount('comments')
-            ->with(['user:id,name,user_name,uuid,image', 'user.media']);
     }
 
     /**
@@ -60,13 +51,5 @@ class Post extends Model implements HasMedia
         $this->addMediaCollection(MediaCollectionName::POST_IMAGE)
             ->singleFile()
             ->useDisk('media');
-    }
-
-    public static function boot()
-    {
-        parent::boot();
-        static::creating(function ($model) {
-            $model->uuid = Str::uuid();
-        });
     }
 }
