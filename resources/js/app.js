a03a0d15130eda8c6c92de96c81d410013ff525e
chayandasgupta <chayandas285178@gmail.com>
2024-10-19 00:42:06 +0600

    add: setup intertia client & server

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,28 @@
-import Alpine from 'alpinejs';
-import { createApp } from 'vue';
+// import Alpine from 'alpinejs';
+// import { createApp } from 'vue';
+// import './bootstrap';
+// import PostComponent from './components/NewsFeed.vue';
+// window.Alpine = Alpine;
+// Alpine.start();
+// createApp(PostComponent).mount("#newsfeed")
 import './bootstrap';
-import PostComponent from './components/NewsFeed.vue';
+
+import Alpine from 'alpinejs';
+
+import { createInertiaApp } from '@inertiajs/vue3';
+import { createApp, h } from 'vue';
+
 window.Alpine = Alpine;
 Alpine.start();
-createApp(PostComponent).mount("#newsfeed")
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    return pages[`./Pages/${name}.vue`]
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,12 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-
-  <!-- AlpineJS CDN -->
-  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-
-  <style>
-    * {
-      font-family: 'Inter', sans-serif;
-    }
-  </style>
-
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body class="bg-gray-100">
-  @include('frontend.partials.navbar')
-
-  <main class="container max-w-xl mx-auto space-y-8 mt-8 px-2 md:px-0 min-h-screen">
-    @yield('content')
-  </main>
-
-  
-  @include('frontend.partials.footer')
-</body>
-
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+  </body>
 </html>
